3d118f1c786c34d4e094ef1b3e59a7f3ed750bfa
f <felipeperez2710@gmail.com>
2017-11-19 01:18:59 +0100

    Se han introducido varios metodos para segmentar y hay que encontrar un metodo para recortar la img con el texto inferior

--- crotal.py
+++ crotal.py
@@ -1,11 +1,12 @@
 import numpy as np
 import cv2
 import math as mt
-import detector_de_texto
+import matplotlib.pyplot as plt
+
 
 class crotal():
     offset = 5
-    def __init__(self,path):
+    def __init__(self,path,method="vecino"):
 
         # Nos interesa tener la imagen a color para luego pintar la recta reconocida
         self.img_color = cv2.imread(path)
@@ -18,13 +19,12 @@ class crotal():
             img_gray = img_gray[0:img_gray.shape[0] - self.offset, :]
 
             # Calculamos su imagen umbralizada
-            self.th = crotal.calcula_umbral(self=self,img=img_gray)
-            _,self.img_umbralizada = cv2.threshold(img_gray,self.th,255,cv2.THRESH_BINARY)
+            self.img_umbralizada = crotal.calcula_img_umbralizada(self=self,img=img_gray,method = method)
 
             # Calculamos el angulo en el que se encuentra girada el crotal y la corregimos
             self.angle = crotal.calcula_angle(self=self,img_umbralizada=self.img_umbralizada,img=self.img_color)
             M = cv2.getRotationMatrix2D((self.img_umbralizada.shape[1] / 2, self.img_umbralizada.shape[0] / 2), self.angle, 1)
-            self.img_corregida = cv2.warpAffine(self.img_color,M,(self.img_umbralizada.shape[1],self.img_umbralizada.shape[0]))
+            self.img_corregida = cv2.warpAffine(img_gray,M,(self.img_umbralizada.shape[1],self.img_umbralizada.shape[0]))
 
             # Una vez corregida la imagen pasamos a reconocer el texto
             self.text = crotal.recognition_text(self=self,img=self.img_corregida)
@@ -32,20 +32,34 @@ class crotal():
 
 
 
-    def calcula_umbral(self,img):
+    def calcula_img_umbralizada(self,img,method):
 
         self.hist = cv2.calcHist([img], [0], None, [255], [0, 255])
-        #Bucle
-        vecino = 5
-        maximos =np.array([])
-        for i in range(0+vecino,255-vecino):
-            value = self.hist[i]
-            rango = self.hist[i-vecino:i+vecino]
-            rang = np.delete(rango,[vecino])
-            if value >np.amax(rang):
-                maximos = np.append(maximos,i)
+        if method == "vecino":
+            #Bucle
+            vecino = 5
+            maximos =np.array([])
+            for i in range(0+vecino,255-vecino):
+                value = self.hist[i]
+                rango = self.hist[i-vecino:i+vecino]
+                rang = np.delete(rango,[vecino])
+                if value >np.amax(rang):
+                    maximos = np.append(maximos,i)
 
-        return  ((maximos[1] + maximos[2]) / 2)
+            self.th = ((maximos[1] + maximos[2]) / 2)
+            _,img_umbralizada = cv2.threshold(img, self.th, 255, cv2.THRESH_BINARY)
+
+        if method == "adaptative":
+            img_umbralizada = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                                    cv2.THRESH_BINARY, 11, 2)
+            kernel = np.ones((3,3),np.uint8)
+            img_umbralizada = cv2.erode(img_umbralizada,kernel,iterations=0)
+            self.th = 0
+
+        if method == "otsu":
+            self.th, img_umbralizada = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        return img_umbralizada
 
 
 
@@ -80,7 +94,26 @@ class crotal():
         return angle
 
     def recognition_text(self,img):
-        boxes = detector_de_texto.text_detection(img)
+
+        #TODO: Crear BB para el texto
+        M = cv2.getRotationMatrix2D((self.img_umbralizada.shape[1] / 2, self.img_umbralizada.shape[0] / 2), self.angle,
+                                    1)
+        umbralizada_corregida = cv2.warpAffine(self.img_umbralizada, M, (self.img_umbralizada.shape[1], self.img_umbralizada.shape[0]))
+        plt.imshow(umbralizada_corregida,cmap="gray"),plt.show()
+
+
+        for i in range(0,umbralizada_corregida.shape[0]):
+            cambio = 0
+            print(i)
+            for j in range(0,umbralizada_corregida.shape[1]-1):
+                print(cambio)
+                if umbralizada_corregida[umbralizada_corregida.shape[0]-i-1,j]!=umbralizada_corregida[i,j+1]:
+                    cambio+=1
+            if cambio == 2:
+                cv2.line(self.img_color,(1,i),(100,i))
+
+
+        #TODO: Pasar tesseract
         return "0288"
 
 
--- practica1.py
+++ practica1.py
@@ -16,13 +16,13 @@ if __name__ == "__main__":
 
     ################ Prueba con un solo crotal ####################
 
-    microtal = crotal('Muestra/Crotal1.TIF')
+    microtal = crotal(path='Muestra/Crotal1.TIF')
     #pintamos los resultados del algoritmo
     fig,axes = plt.subplots(nrows=2,ncols=2)
     axes[0, 0].imshow(microtal.img_color,cmap="gray")
     axes[0, 0].set_title("Imagen")
     axes[0, 1].hist(microtal.hist, 256, [0, 256])
-    axes[0, 1].hist(microtal.th,1000,[0,256],color = 'y')
+    axes[0, 1].hist(microtal.th,100,[0,256],color = 'y')
     axes[0, 1].set_title("Histrograma")
     axes[1, 0].imshow(microtal.img_umbralizada, cmap="gray")
     axes[1, 0].set_title("Imagen umbralizada")
@@ -32,69 +32,73 @@ if __name__ == "__main__":
 
     print("Se ha detectado: ",microtal.text)
 
+
+
     #######################   Test ################################
-    reader = list(csv.reader(open("CrotalesTest/GroundTruth.csv")))
+    test = False
+    if test == True:
+        reader = list(csv.reader(open("CrotalesTest/GroundTruth.csv")))
 
 
-    # Crotales en el que no se ha reconocido ningun texto
-    Nempty = 0
-    empty = np.array([])
+        # Crotales en el que no se ha reconocido ningun texto
+        Nempty = 0
+        empty = np.array([])
 
-    # Crotales en los que el texto reconocido no coincide
-    Nfail = 0
-    fail = np.array([])
+        # Crotales en los que el texto reconocido no coincide
+        Nfail = 0
+        fail = np.array([])
 
-    # Crotales reconocido con exito
-    Nwin = 0
-    win = np.array([])
-    i = 0
+        # Crotales reconocido con exito
+        Nwin = 0
+        win = np.array([])
+        i = 0
 
-    print("----------- Iniciamos test -----------")
-    for infile in sorted(glob.glob((path_to_directory + '/*TIF'))):
-        i+=1
-        file, ext = os.path.splitext(infile)
-        print("Para el crotal: ",infile)
-        microtal = crotal(infile)
+        print("----------- Iniciamos test -----------")
+        for infile in sorted(glob.glob((path_to_directory + '/*TIF'))):
+            i+=1
+            file, ext = os.path.splitext(infile)
+            print("Para el crotal: ",infile)
+            microtal = crotal(infile)
 
-        #No se ha reconocido texto
-        if not microtal.text:
+            #No se ha reconocido texto
+            if not microtal.text:
 
-            print("\tNo se ha reconocido texto")
-            Nempty += 1
-            empty = np.append(empty,i)
-
-
-        elif microtal.text == reader[i][1]:
-            print("\tEl algoritmo ha detectado: ", microtal.text)
-            print("\tEl texto coincide con ", reader[i][1])
-            Nwin += 1
-            win = np.append(win,i)
-
-        else:
-            print("\tEl algoritmo ha detectado: ",microtal.text)
-            print("\tEl texto NO coincide con ",reader[i][1])
-            Nfail+=1
-            fail = np.append(fail,i)
+                print("\tNo se ha reconocido texto")
+                Nempty += 1
+                empty = np.append(empty,i)
 
 
-    print("\n----------- Resultados -----------")
+            elif microtal.text == reader[i][1]:
+                print("\tEl algoritmo ha detectado: ", microtal.text)
+                print("\tEl texto coincide con ", reader[i][1])
+                Nwin += 1
+                win = np.append(win,i)
 
-    print("Numero de vacios: ",Nempty,"-> ",Nempty/i,"%")
-    print("Numero de fallos: ",Nfail,"-> ",Nfail/i,"%")
-    print("Numero de aciertos: ",Nwin,"-> ",Nwin/i,"%")
+            else:
+                print("\tEl algoritmo ha detectado: ",microtal.text)
+                print("\tEl texto NO coincide con ",reader[i][1])
+                Nfail+=1
+                fail = np.append(fail,i)
 
 
-    # Pintamos en un grafico de barras los resultados
-    N = 3
-    ind = np.arange(N)
+        print("\n----------- Resultados -----------")
 
-    data = [Nempty,Nfail,Nwin]
+        print("Numero de vacios: ",Nempty,"-> ",Nempty/i,"%")
+        print("Numero de fallos: ",Nfail,"-> ",Nfail/i,"%")
+        print("Numero de aciertos: ",Nwin,"-> ",Nwin/i,"%")
 
-    fig, ax = plt.subplots()
 
-    plt.bar(ind,data)
-    plt.xticks(ind,('Vacio', 'Fallos', 'Aciertos'))
-    plt.show()
+        # Pintamos en un grafico de barras los resultados
+        N = 3
+        ind = np.arange(N)
+
+        data = [Nempty,Nfail,Nwin]
+
+        fig, ax = plt.subplots()
+
+        plt.bar(ind,data)
+        plt.xticks(ind,('Vacio', 'Fallos', 'Aciertos'))
+        plt.show()
 
 
 
