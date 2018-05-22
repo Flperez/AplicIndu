c3339b2be0e0f9f611ba7cc365730fd8e748ea8f
f <felipeperez2710@gmail.com>
2018-05-22 17:23:19 +0200

    download model

--- P3/training/models/download_coco.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2017_11_17.tar.gz
-tar -zxvf ssd_mobilenet_v1_coco_2017_11_17.tar.gz
-rm -rf ssd_mobilenet_v1_coco_2017_11_17.tar.gz
-
